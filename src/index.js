420d69506ef48324c0596a9d43a542b101d49aaa
berthahsu <271181958@qq.com>
2022-01-05 20:31:12 +0800

    函数组件与类组件

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import "./style.css";
+function  App(){
+  return(
+    <div className='App'>
+      我是爸爸
+      <Son1 name="  大儿子你好！"/>
+      <Son2 name="  小儿子你好！"/>
+    </div>
+  )
+}
+/* 函数组件 */
+function Son1(props){
+  const [n,setN]=React.useState(0)
+  return (
+    <div className='Son1'>
+      我是大儿子，爸爸对我说:{props.name}，n:{n}
+      <button onClick={()=>setN(n+1)}>+1</button>
+    </div>
+  )
+}
+/*类组件  */
+class Son2 extends React.Component{
+  constructor(){
+    super()
+    this.state={ n:0 }
+  }
+  add() {
+    this.setState({n:this.state.n+1})
+  }
+  render(){
+    return (
+      <div className='Son2'>
+        我是小儿子，爸爸对我说:{this.props.name}，n:{this.state.n}
+        <button onClick={()=>this.add()}>+1</button>
+      </div>
+    )
+  }
+}
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />,document.getElementById('root'));
